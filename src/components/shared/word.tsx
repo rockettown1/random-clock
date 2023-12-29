eb2e9c2874bbd9168aee1ebab7e8fdcf1c58d951
Dan Krishnan <dhani.krishnan@theverygroup.com>
2023-12-29 17:32:04 +0000

    chore: update

--- src/components/shared/word.tsx
+++ src/components/shared/word.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import { useHighlight } from "../shared/use-highlight";
 import { toGlow, fromGlow } from "./animations";
